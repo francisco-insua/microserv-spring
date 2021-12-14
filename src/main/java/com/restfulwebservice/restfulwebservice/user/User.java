38b20df5d16e911eb1cb8b7a3c0870b716b6a354
Francisco Insua <finsua@fi.uba.ar>
2021-12-14 02:28:28 -0300

    Adding JPA Features

--- src/main/java/com/restfulwebservice/restfulwebservice/user/User.java
+++ src/main/java/com/restfulwebservice/restfulwebservice/user/User.java
@@ -1,22 +1,37 @@
 package com.restfulwebservice.restfulwebservice.user;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
 
-    @Size(min=2, message = "Name should have at least 2 characters")
+    @Size(min=2, message="Name should have atleast 2 characters")
+    //@ApiModelProperty(notes="Name should have atleast 2 characters")
     private String name;
 
     @Past
+    //@ApiModelProperty(notes="Birth date should be in the past")
     private Date birthDate;
+
+    @OneToMany(mappedBy="user")
+    private List<Post> posts;
 
     protected User() {
 
     }
+
     public User(Integer id, String name, Date birthDate) {
+        super();
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
@@ -46,12 +61,16 @@ public class User {
         this.birthDate = birthDate;
     }
 
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", birthDate=" + birthDate +
-                '}';
+        return String.format("User [id=%s, name=%s, birthDate=%s]", id, name, birthDate);
     }
 }
